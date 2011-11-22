25d89ad9157176f4a3e1c3d8c7c656f9c0d2f403
Ryan Svihla <rssvihla@gmail.com>
2011-11-21 20:38:58 -0600

    setup gems

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function searchLocation(eventHandler){
+
+}
+function addLocation(){
+
+}
+function removeLocation(){
+
+}
+function checkout(){
+
+}
+
+$(function(){
+    $('#search_button').click(search_location);
+    $('#add_location_button').click(addLocation);
+    $('#remove_location_button').click(removeLocation);
+    $('#checkout_button').click(checkout);
+});
