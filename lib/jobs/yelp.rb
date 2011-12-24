88a5bedf2c68c377f1d9a0574d2faa7995f19431
Ryan Svihla <rssvihla@gmail.com>
2011-12-23 22:12:52 -0600

    commented named and organized

--- lib/jobs/yelp.rb
+++ lib/jobs/yelp.rb
@@ -31,19 +31,54 @@ class Parser
   
 private
 
-  def parse_restaurants_count()
-      #had trouble with this section so ended up copying it
-      td = Nokogiri::HTML(open(@all_restaurants_link)).css("table.fs_pagination_controls tr td:first-of-type span").first
-      td.inner_text.split(' ')[4].to_i
-  end
-  
+  #TODO: don't use silly browser agent. Try IE or something more common,
   def read_html(url)
       hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
       my_html = ""
       open(url, hdrs).each {|s| my_html << s}
       return my_html
-  end  
+  end
 
+  #TODO: move to more of a paging concept. Should have a method get "total pages" instead of "get resturaunts count"
+  #TODO: rename all references from "resturaunts" to "location"
+  def parse_restaurants_count()
+      #had trouble with this section so ended up copying it
+      td = Nokogiri::HTML(open(@all_restaurants_link)).css("table.fs_pagination_controls tr td:first-of-type span").first
+      td.inner_text.split(' ')[4].to_i
+  end
+
+  #need a walkthrough on how this happens
+  # think of index as page if page is zero based (first page is 0, second page is 1 and so on)
+  #TODO: rename the variable "index" to a variable named "page"
+  def parse_restaurants_list(index="")
+    found_restaurants = Array.new
+    Nokogiri::HTML(open("#{@all_restaurants_link}#{index}")).css("div.businessresult") do |location|
+      title_link = location.css("h4.itemheading a").first
+      found = Location.new(:url=>title_link[:href],:name=>title_link.inner_html.sub(/[0-9]*\./,''))
+      found_restaurants << found
+    end
+    found_restaurants
+  end
+
+
+
+  #used to parse all reviews of a location
+  def parse_all_sections(location)
+    count = parse_review_count(location)
+    start_index = 0
+    while(start_index < count)
+      parse_section(location, start_index)
+      start_index = start_index + 40
+    end
+  end
+  
+  #used to parse the review count so we now how many pages are available for a review
+  #TODO: move all the page count logic to another method which does the math and makes the page concept more explicit
+  def parse_review_count(location)
+    Nokogiri::HTML(open("http://www.yelp.com#{location.url}")).css("span.review-count span.count").inner_text.to_i
+  end
+  
+  #TODO: rename to parse review
   def parse_section(location, index)
     my_html = read_html "http://www.yelp.com/#{location[:url]}=#{index}"
     doc = Nokogiri::HTML(open(my_html))
@@ -57,7 +92,8 @@ private
       parsed_review.save!
     end
   end
-
+  
+  #TODO: this is temporary. should be replaced with a method to parse several locations
   def parse_satx_restaurants()
     found_restaurants = Array.new
     job_start_time = Time.now
@@ -71,30 +107,6 @@ private
     end
     puts "total time to parse restuarant list was #{Time.now - job_start_time}"
     found_restaurants
-  end
-  
-  #need a walkthrough on how this happens
-  def parse_restaurants_list(index="")
-    found_restaurants = Array.new
-    Nokogiri::HTML(open("#{@all_restaurants_link}#{index}")).css("div.businessresult") do |location|
-      title_link = location.css("h4.itemheading a").first
-      found = Location.new(:url=>title_link[:href],:name=>title_link.inner_html.sub(/[0-9]*\./,''))
-      found_restaurants << found
-    end
-    found_restaurants
-  end
-
-  def parse_review_count(location)
-    Nokogiri::HTML(open("http://www.yelp.com#{location.url}")).css("span.review-count span.count").inner_text.to_i
-  end
-
-  def parse_all_sections(location)
-    count = parse_review_count(location)
-    start_index = 0
-    while(start_index < count)
-      Yelp::parse_section(location, start_index)
-      start_index = start_index + 40
-    end
   end
 
 end
