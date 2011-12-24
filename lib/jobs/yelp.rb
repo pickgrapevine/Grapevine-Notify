96ee3c5aa179698829d6a7878f5bd5df280f942a
Richard Ortega <richardjortega@gmail.com>
2011-12-23 19:39:46 -0600

    Updated to be similar to Ryan's yelp parser, having difficulty running 'rake parsers:yelp'

--- lib/jobs/yelp.rb
+++ lib/jobs/yelp.rb
@@ -2,10 +2,18 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'date'
+require 'bigdecimal'
 require_relative "../../app/models/review"
 require_relative "../../app/models/location"
 
 module Yelp
+  
+def self.read_html(url)
+    hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
+    my_html = ""
+    open(url, hdrs).each {|s| my_html << s}
+    return my_html
+end  
 
 def self.parse_section(location, index)
   url = "http://www.yelp.com/#{location[:url]}=#{index}"
@@ -22,32 +30,61 @@ def self.parse_section(location, index)
 end
 
 def self.parse_satx_restaurants()
+  found_restaurants = Array.new
   job_start_time = Time.now
   page_count = parse_restaurants_count
+  start_index = 0
+  while(start_index  < count)do
+    start_parse = Time.now
+    found_restaurants << parse_restaurants_list(start_index)
+    puts "parsed index #{start_index} elapsed #{(Time.now - start_parse)}"
+    start_index = start_index + 10
+  end
+  puts "total time to parse restuarant list was #{Time.now - job_start_time}"
+  found_restaurants
 end
 
 class Parser
   def initialize()
     @all_restaurants_link = "http://www.yelp.com/search?find_loc=San+Antonio%2C+TX&cflt=restaurants#start="
     @san_antonio = Yelp::parse_satx_restaurants
+    @customers = [{:name=>"El Milagrito",:url=>"/biz/el-milagrito-cafe-san-antonio?rpp=40&sort_by=date_desc&start="}]
   end
 
   #copied verbatim
   def all
     @san_antonio.each do |sa_biz|
-      parse_all_sections(sa_biz)
+      parse_all_reviews(sa_biz)
     end
+  end
+  
+  #currently not implemented in rake task, need walkthrough
+  def customers
+    @customers.each do |customer|
+     parse_all_reviews(customer)
+     end
   end
     
 private
-  def parse_review_count(location)
-    Nokogiri::HTML(open("http://www.yelp.com#{location.url}")).css("span.review-count span.count").inner_text.to_i
+  #need a walkthrough on how this happens
+  def parse_restaurants_list(index="")
+    found_restaurants = Array.new
+    Nokogiri::HTML(open("#{@all_restaurants_link}#{index}")).css("div.businessresult") do |location|
+      title_link = location.css("h4.itemheading a").first
+      found = Location.new(:url=>title_link[:href],:name=>title_link.inner_html.sub(/[0-9]*\./,''))
+      found_restaurants << found
+    end
+    found_restaurants
   end
 
-  def parse_restaurants_count
+  def parse_restaurants_count()
     #had trouble with this section so ended up copying it
-    td = Nokogiri::HTML(open(@all_restaurants_link)).search("table.fs_pagination_controls tr td:first-of-type span").first
+    td = Nokogiri::HTML(open(@all_restaurants_link)).css("table.fs_pagination_controls tr td:first-of-type span").first
     td.inner_text.split(' ')[4].to_i
+  end
+
+  def parse_review_count(location)
+    Nokogiri::HTML(open("http://www.yelp.com#{location.url}")).css("span.review-count span.count").inner_text.to_i
   end
 
   def parse_all_reviews(location)
--- lib/tasks/yelp_parser_task.rake
+++ lib/tasks/yelp_parser_task.rake
@@ -3,8 +3,7 @@ require_relative "../jobs/yelp"
 namespace :parsers do
   desc "Parse Yelp Reviews"
   task :yelp => :environment do
-    #Yelp::parse_all_reviews
-    Yelp::parse_restaurants_count
-    #puts "hello world!"
+    #Parse All Yelp San Antonio Restaurants
+      Yelp::Parser.new.all
   end
 end
