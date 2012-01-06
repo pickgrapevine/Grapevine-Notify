c99370d14526d62c5dc17f7eb7e63146c12e7f71
Richard Ortega <richardjortega@gmail.com>
2012-01-05 23:41:47 -0600

    This will help from having same data across all computers, it's only one record for locations and one record for reviews (but it has associating foreign key). It'll blow any data in your sqlite3, reset PK IDs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Deleting all data and resetting primary key IDs"
+Rake::Task['db:migrate:reset'].invoke
+puts "Done"
+
+puts "Creating single Review entry"
+Review.create [{:id => '1',
+			   :author => 'Richard Ortega',
+			   :author_location => 'San Antonio, TX',
+			   :recommend => 'Yes',
+			   :rating => '5',
+			   :comment => 'Loved the food at Zinc, it was amazing!',
+			   :date => '2001-02-03T03:04:05+03:00',
+			   :link => 'biz/zinc-bistro-and-wine-bar-san-antonio',
+			   :location_id => '1'}]
+puts "Review Data - Done"
+
+puts "Creating single Location entry"
+Location.create [{:id => '1',
+			   :address_line_1 => '207 North Presa',
+			   :address_line_2 => '',
+			   :address_line_3 => '',
+			   :city => 'San Antonio',
+			   :state => 'TX',
+			   :zip => '78205',
+			   :phone_number_1 => '(210) 224-2900',
+			   :phone_number_2 => '',
+			   :phone_number_3 => '',
+			   :name => 'Zinc Bistro & Wine Bar',
+			   :url => '/biz/zinc-bistro-and-wine-bar-san-antonio'}]
+puts "Location Data - Done"
