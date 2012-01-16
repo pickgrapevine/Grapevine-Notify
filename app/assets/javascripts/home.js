001e4ec738c4999e3f59886845bb81296ce2582a
Ryan Svihla <rssvihla@gmail.com>
2012-01-16 12:35:26 -0600

    autocomplete works. still needs styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,24 @@
 $(function(){
+  function appendItemIfNotNullOrEmpty(data, data_item){
+    if(data_item){
+      data.append('<br/>' +data_item);
+    }
+    return data;
+  }
   //live is deprecated and this needs to be moved to "on" 
   $("#location_search").live('keyup.autocomplete', function(){
     $(this).autocomplete({
       source: "/location/index",
-      minLength: 2
-    });
+      minLength: 2,
+    })
+    .data( "autocomplete" )._renderItem = function( ul, item ) {
+     var li =  $( "<li></li>" )
+      .data( "item.autocomplete", item )
+      .append( "<a>" + item.label + "<br/>" + item.address_line_1);
+      li = appendItemIfNotNullOrEmpty(li, item.address_line_2);
+      li.append("</a>").appendTo( ul );
+      return li;
+    };
+
   });
 });
